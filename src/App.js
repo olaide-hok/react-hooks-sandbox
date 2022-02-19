c3081c6b033183b2a983dd4bc8205fc39baa29ea
olaide-hok <olaidekareem43@yahoo.com>
2022-02-19 13:34:56 +0100

    create DOM Reference with useRef Hook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import UseRefExample1 from "./components/UseRefExample1";
 
 function App() {
   return (
     <div className="container mt-5">
-      
+      <UseRefExample1 />  
     </div>
   );
 }
