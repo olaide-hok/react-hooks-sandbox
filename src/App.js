f1b1ae9655efc9b2fc3293dcc36a4b09f5a559d8
olaide-hok <olaidekareem43@yahoo.com>
2022-02-19 15:02:57 +0100

    UseMemo hook for a dummy expensive function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // import UseRefExample1 from "./components/UseRefExample1";
 // import UseRefExample2 from "./components/UseRefExample2";
-import UseRefExample3 from "./components/useRefExample3";
+// import UseRefExample3 from "./components/useRefExample3";
+
+import UseMemoExample from "./components/UseMemoExample";
 
 function App() {
   return (
     <div className="container mt-5">
-      <UseRefExample3 />  
+      <UseMemoExample />  
     </div>
   );
 }
